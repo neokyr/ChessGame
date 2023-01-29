9949371758b35dc8936b455fe7a0f48a774a2d0c
Paul-Antoine <paul-antoine@desanti.me>
2023-01-29 10:27:47 +0100

    Add message box on new game

--- src/main.cpp
+++ src/main.cpp
@@ -22,21 +22,47 @@ int main(int argc, char** argv) {
     bool continueGame = true;
     SDL_Event event;
     int x = 0;
-    Button new_game(600, 300, 200, 50,
-                    "../assets/btn_nouv_jeu_1.png", "../assets/btn_nouv_jeu_2.png",
+    Button new_game(600, 310, 200, 40,
+                    "../assets/button_new-game.png", "../assets/button_new-game1.png",
+                    [&main_window]() {
+                        SDL_MessageBoxData data;
+                        data.title = "New Game";
+                        data.flags = 0;
+                        data.message = "Are you sure?";
+                        data.numbuttons = 2;
+                        data.window = main_window->getWindow();
+                        SDL_MessageBoxButtonData button[2];
+                        button[0].flags = SDL_MESSAGEBOX_BUTTON_RETURNKEY_DEFAULT;
+                        button[0].buttonid = 0;
+                        button[0].text = "Yes";
+                        button[1].flags = SDL_MESSAGEBOX_BUTTON_ESCAPEKEY_DEFAULT;
+                        button[1].buttonid = 1;
+                        button[1].text = "No";
+                        data.buttons = button;
+                        int buttonid;
+                        if( SDL_ShowMessageBox(&data, &buttonid) < 0 ) {
+                            // error occurred
+                        } else if( buttonid == 0 ) {
+                            // user clicked yes
+                        } else if( buttonid == 1 ) {
+                            // user clicked no
+                        }
+                    });
+    Button cancel_move(600, 351, 200, 40,
+                    "../assets/button_cancel-move.png", "../assets/button_cancel-move1.png",
                     [&x]() {
-                        SDL_ShowSimpleMessageBox(0, "Test", "Bonjour le monde", Window::getMainWindow()->getWindow());
                     });
 
 
-    BoardWidget board(0,0,600);
-    ScrollableWidget histo_test(600,0,200, 300, "../assets/scr_background.png", 10);
+    BoardWidget board(10,10,590);
+    ScrollableWidget histo_test(600,10,200, 300, "../assets/scr_background.png", 10);
     SDL_Texture * bg = main_window->loadImg((string) "../assets/background.png");
 
     while(continueGame) {
         SDL_RenderClear(main_window->getRender());
         SDL_RenderCopy(main_window->getRender(), bg, nullptr, nullptr);
         new_game.print();
+        cancel_move.print();
         histo_test.print();
         board.print();
 
