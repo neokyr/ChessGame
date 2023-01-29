0c69f8f04e340700aac813fdedb91caa3117c095
Paul-Antoine <paul-antoine@desanti.me>
2023-01-29 10:28:23 +0100

    Fix BoardWidget didn't account for widget pos on screen

--- src/Window/BoardWidget.cpp
+++ src/Window/BoardWidget.cpp
@@ -40,27 +40,26 @@ void BoardWidget::print() {
     int h_case = (board->h - font_space) / 8;
 
     for (int i = 7 ; i >= 0; i--) {
-        int x = 0 + (font_space - w_font) /2 ;
-        int y = (7-i) * h_case + (h_case - h_font) /2;
+        int x = board->x + (font_space - w_font) /2;
+        int y = board->y + (7-i) * h_case + (h_case - h_font) /2;
         SDL_Rect r = {x, y, w_font, h_font};
         SDL_RenderCopy(win->getRender(), letters_[i], nullptr, &r);
     }
 
     for (int i = 0 ; i < 8; i++) {
-        int x = i * w_case + font_space + (w_case - w_font) /2;
-        int y = board->h - font_space + (font_space - h_font) /2;
+        int x = board->x + i * w_case + font_space + (w_case - w_font) /2;
+        int y = board->y + board->h - font_space + (font_space - h_font) /2;
         SDL_Rect r = {x, y, w_font, h_font};
         SDL_RenderCopy(win->getRender(), numbers_[i], nullptr, &r);
     }
 
     for (int i = 0; i <64; ++i) {
         bool is_white_case = (i%2 + i/8) %2;
-        int x = (i%8) * w_case + font_space;
-        int y = (i/8) * h_case;
+        int x = (i%8) * w_case + font_space + board->x;
+        int y = (i/8) * h_case + board->y;
         SDL_Rect r = {x, y, w_case, h_case};
         if(is_white_case) {
             SDL_RenderCopy(win->getRender(), w_case_0_, nullptr, &r);
-            SDL_RenderCopy(win->getRender(), w_piece_[KING], nullptr, &r);
         } else {
             SDL_RenderCopy(win->getRender(), b_case_0_, nullptr, &r);
             SDL_RenderCopy(win->getRender(), w_piece_[KING], nullptr, &r);
