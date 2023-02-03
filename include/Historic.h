13b8bdc567d8465a0816f28856afb3efb6807e33
Paul-Antoine <paul-antoine@desanti.me>
2023-02-03 17:25:36 +0100

    Change historic

--- include/Historic.h
+++ include/Historic.h
@@ -7,19 +7,22 @@
 
 using namespace std;
 
+enum MoveType {NORMAL, LCast, RCast};
+
 class Historic {
 private:
     Piece* moving_;
     pair<int,int> from_;
     pair<int,int> to_;
     Piece* destroyed_;
+    MoveType moveType_;
 
 public:
     Historic() = default;
     Historic(Historic const& h) = default;
     Historic& operator=(Historic const& h) = default;
 
-    Historic(Piece* moving, int x1, int y1, int x2, int y2, Piece* destroyed);
+    Historic(Piece* moving, int x1, int y1, int x2, int y2, Piece* destroyed = nullptr, MoveType moveType = NORMAL);
 
     Piece *getMoving() const;
     const pair<int, int> &getFrom() const;
